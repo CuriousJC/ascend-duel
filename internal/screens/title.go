b730a15300c5102781ccff1f73889482b23de8e2
Justin Crosby <justin.crosby@gmail.com>
2025-01-03 12:54:00 -0500

    some image work for a button

--- internal/screens/title.go
+++ internal/screens/title.go
@@ -37,11 +37,21 @@ func DrawTitleScreen(screen *ebiten.Image, gs *state.GlobalState) {
 	ThirdTextOp.GeoM.Translate(0, 200)
 	text.Draw(screen, "BOUNDS TESTING", &text.GoTextFace{Source: gs.Fonts["kubasta"], Size: 20}, ThirdTextOp)
 
-	vector.DrawFilledRect(screen, 100, 100, 200, 200, color.RGBA{R: 255, G: 0, B: 0, A: 255}, false)
-
-	vector.StrokeCircle(screen, 300, 300, 50, 5, color.RGBA{R: 0, G: 255, B: 0, A: 255}, false)
-
-	//TODO:  Keep playing with vector stuff:
-	//https://ebitengine.org/en/examples/vector.html
+	//TODO:  Move this to a shape or a common or something
+	// button creation using vector and new 100,100 image with text being placed upon it
+	img := ebiten.NewImage(100, 100)
+	vector.DrawFilledRect(img, 0, 0, 100, 100, color.RGBA{R: 255, G: 0, B: 0, A: 255}, false)
+	vector.StrokeCircle(img, 50, 50, 25, 5, color.RGBA{R: 0, G: 255, B: 0, A: 255}, false)
+	centerButtonTextOp := &text.DrawOptions{}
+	centerButtonTextOp.GeoM.Translate(50, 50)
+	text.Draw(img, "BUTTON", &text.GoTextFace{Source: gs.Fonts["kubasta"], Size: 20}, centerButtonTextOp)
+	if gs.CountSecond%2 == 0 {
+		screen.DrawImage(img, &ebiten.DrawImageOptions{})
+	} else {
+		mover := &ebiten.DrawImageOptions{}
+		mover.GeoM.Invert()
+		mover.GeoM.Translate(200, 200)
+		screen.DrawImage(img, mover)
+	}
 
 }
